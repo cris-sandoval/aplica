ec1b24e1d898b5ea52acc6fbbff032e7c84fe368
unknown <cris.sandoval.cf@gmail.com>
2023-06-29 00:04:45 -0300

    lista la calculadora

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const displayValorAnterior = document.getElementById('valor-anterior')
 const displayValorActual = document.getElementById('valor-actual')
-const operador = document.querySelectorAll('.operador')
+const botonesOperadores = document.querySelectorAll('.operador')
 const botonesNumeros = document.querySelectorAll('.numero')
 
-const display = new Display(displayValorActual, displayValorAnterior)
+const display = new Display(displayValorAnterior, displayValorActual)
 
 botonesNumeros.forEach(boton => {
     boton.addEventListener('click',() =>  display.agregarNumero(boton.innerHTML))
+})
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', ()=>display.computar(boton.value) )
 })
